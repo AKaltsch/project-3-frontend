1dcf945c81342f318ec69cfe69f1e2c4f10b8d43
Sam Shearer <sshearer101@gmail.com>
2021-09-15 17:42:49 -0600

    new-braanch

--- fantasy-frontend/src/App.js
+++ fantasy-frontend/src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 import { useState, useEffect } from 'react'
 import PlayerContainer from './components/PlayerContainer'
 import TeamOneContainer from './components/TeamOneContainer'
 import TeamTwoContainer from './components/TeamTwoContainer'
-import TeamContainer from './components/TeamContainer';
+import TeamContainer from './components/TeamContainer'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 const myAPI = 'http://localhost:9292'
 
 function App() {
-
   const [players, setPlayers] = useState([])
 
   const [teamOne, setTeamOne] = useState([])
@@ -18,46 +18,75 @@ function App() {
   const [teams, setTeams] = useState([])
 
   function addToTeamOne(playerToAdd) {
-    setTeamOne(([...teamOne, playerToAdd]))
+    setTeamOne([...teamOne, playerToAdd])
     console.log(teamOne)
   }
 
   function addToTeamTwo(playerToAdd) {
-    setTeamTwo(([...teamTwo, playerToAdd]))
+    setTeamTwo([...teamTwo, playerToAdd])
     console.log(teamTwo)
   }
 
-  function handleSubmit([arr1, arr2]) {
-    fetch(myAPI, {
-      method: "POST",
-      headers: {
-        Accept: "*/*",
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify([arr1, arr2]),
-    })
-    .then(res => res.json())
-    .then(([arr1, arr2]) => setTeams([...teams, [arr1, arr2]]))
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(e)
   }
 
-  useEffect(() =>
-    fetch(`${myAPI}/players`)
-      .then(res => res.json())
-      .then(data => setPlayers(data)),
-    [])
+  // function handleSubmit() {
+  //   fetch(myAPI, {
+  //     method: 'POST',
+  //     headers: {
+  //       Accept: '*/*',
+  //       'Content-type': 'application/json',
+  //     },
+  //     body: JSON.stringify([arr1, arr2]),
+  //   })
+  //     .then((res) => res.json())
+  //     .then(([arr1, arr2]) => setTeams([...teams, [arr1, arr2]]))
+  // }
 
-
+  useEffect(
+    () =>
+      fetch(`${myAPI}/players`)
+        .then((res) => res.json())
+        .then((data) => setPlayers(data)),
+    [],
+  )
 
   return (
-    <div className="App">
-      <h1> The Flatiron Grid-Iron</h1>
-      <TeamContainer teamOne={teamOne} teamTwo={teamTwo} />
-      <h2>Draftable Players:</h2>
-      <div className="Player-Container">
-        <PlayerContainer players={players} addToTeamOne={addToTeamOne} addToTeamTwo={addToTeamTwo} />
+    <Router>
+      <div className="App">
+        <header className="app-header">
+          <Link to="/"> Fantasy </Link>|<Link to="/teams"> Teams </Link>|
+          <Link to="/players"> Players </Link>
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <h1> The Flatiron Grid-Iron</h1>
+            </Route>
+            <Route path="/teams">
+              <TeamContainer
+                teamOne={teamOne}
+                teamTwo={teamTwo}
+                handleSubmit={handleSubmit}
+              />
+            </Route>
+            <Route path="/players">
+              <h2>Draftable Players:</h2>
+              <div className="player-container">
+                <PlayerContainer
+                  players={players}
+                  addToTeamOne={addToTeamOne}
+                  addToTeamTwo={addToTeamTwo}
+                />
+              </div>
+            </Route>
+          </Switch>
+        </main>
       </div>
-    </div>
-  );
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- fantasy-frontend/src/components/TeamContainer.js
+++ fantasy-frontend/src/components/TeamContainer.js
@@ -1,18 +1,23 @@
-import TeamOneContainer from "./TeamOneContainer";
-import TeamTwoContainer from "./TeamTwoContainer";
+import TeamOneContainer from './TeamOneContainer'
+import TeamTwoContainer from './TeamTwoContainer'
 
-export default function TeamContainer({ teamOne, teamTwo }) {
-    return (
-        <div>
-        <h2>Team One</h2>
-        <div className="Team1-Container">
-          <TeamOneContainer players={teamOne} />
-        </div>
+export default function TeamContainer({ teamOne, teamTwo, handleSubmit }) {
+  return (
+    <div>
+      <form onSubmit={() => handleSubmit()}>
+      <h2>Team One</h2>
+        <label htmlFor="team-one">Team One</label>
+          <div className="Team1-Container">
+            <TeamOneContainer players={teamOne} />
+          </div>
+
         <h2>Team Two</h2>
-        <div>
+        <label htmlFor="team-two">Team Two</label>
+        <div className="Team2-Container">
           <TeamTwoContainer players={teamTwo} />
         </div>
-        <button>Submit Teams</button>
-      </div>
-    )
+        <button type="submit">Submit Teams</button>
+      </form>
+    </div>
+  )
 }
